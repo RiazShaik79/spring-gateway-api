13c001219fc11b438136db0086b1873053bd79ef
Latha <Latha@DESKTOP-GP0TL91>
2020-05-08 04:49:11 +0100

    update

--- src/main/java/io/javabrains/GatewayConfiguration.java
+++ src/main/java/io/javabrains/GatewayConfiguration.java
@@ -1,5 +1,6 @@
 package io.javabrains;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @EnableHystrix
 @Configuration
+@RefreshScope
 public class GatewayConfiguration {
 
 	@Bean
